4906d6f7002202edd66e1b0a8d3cc933715b8803
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-11 11:42:42 +0100

    bookmarks model created

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true
+  has_many :bookmarks
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  validates :poster_url, presence: true
+  validtes :rating, numericality: true
 end
