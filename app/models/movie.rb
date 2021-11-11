1c54211dfd7388925b0853d3ac3868185074bd2d
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-11 17:07:22 +0100

    WatchList some features added, not finished

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  validates :poster_url, presence: true
-  validtes :rating, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+buffer = URI.open(url)
+results = JSON.parse(buffer.read)
+
+results = results['results']
+# puts results
+
+results.first(20).each do |result|
+  Movie.create(
+    title: result['original_title'],
+    overview: result['overview'],
+    poster_url: result['backdrop_path'],
+    rating: result['vote_average']
+  )
+end
+
+5.times do
+  List.create(name: 'List')
+end
+
+5.times do
+  Bookmark.create(comment: 'Great movie!')
+end
